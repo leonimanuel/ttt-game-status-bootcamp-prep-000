6b9a2f6c9c34f602a640251354323eeeec822f11
Leon Malisov <leonmalisov@gmail.com>
2019-11-30 03:26:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,11 +14,9 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
-
-
 def won?(board)
-  winning_combination = false
   winner = false
+  winning_combination = false
   WIN_COMBINATIONS.each do |win_combination|
     if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X" || board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
       winner = true
@@ -53,12 +51,12 @@ end
 
 def winner(board)
   winner = false
-  if board[winning_combination[0]] == "X" && board[winning_combination[1]] == "X" && board[winning_combination[2]] == "X"
-    winner = "X"
-  elsif board[winning_combination[0]] == "O" && board[winning_combination[1]] == "O" && board[winning_combination[2]] == "O"
-    winner = "O"
+  WIN_COMBINATIONS.each do |win_combination|
+    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+      winner = "X"
+    elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+      winner = "O"
+    end  
   end
-  if !!winner
-    winner
-  end
+  winner
 end
