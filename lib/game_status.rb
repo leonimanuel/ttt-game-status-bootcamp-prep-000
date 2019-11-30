9ca819469868ee3f12ebe066c3960df06b4da5f1
Leon Malisov <leonmalisov@gmail.com>
2019-11-30 03:01:52 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -52,12 +52,10 @@ end
 
 def winner(board)
   winner = false
-  WIN_COMBINATIONS.each do |win_combination|
-    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-      winner = "X"
-    elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
-      winner = "O"
-    end
+  if board[winning_combination[0]] == "X" && board[winning_combination[1]] == "X" && board[winning_combination[2]] == "X"
+    winner = "X"
+  elsif board[winning_combination[0]] == "O" && board[winning_combination[1]] == "O" && board[winning_combination[2]] == "O"
+    winner = "O"
   end
   if !!winner
     winner
