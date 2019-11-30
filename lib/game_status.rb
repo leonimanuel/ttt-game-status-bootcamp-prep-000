316b3bf1b4b25b4433f39b9c1b614514b75e69d0
Leon Malisov <leonmalisov@gmail.com>
2019-11-30 02:06:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,6 +18,9 @@ def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     if board(win_combination[0]) == "X" && board(win_combination[1]) == "X" && board(win_combination[2]) == "X"
       puts "X WINS"
+    else
+      false
     end
+    
   end
 end
