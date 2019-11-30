869687b58c3a828f1b0498143b1b82bc6310b73b
Leon Malisov <leonmalisov@gmail.com>
2019-11-30 02:12:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,11 +15,13 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 def won?(board)
+winner = false
   WIN_COMBINATIONS.each do |win_combination|
     if board(win_combination[0]) == "X" && board(win_combination[1]) == "X" && board(win_combination[2]) == "X"
       puts "X WINS"
+      winner = true
       break
     end
-    break
   end
+  
 end
