c2cb6494803e2b1cff9fb9697a4e8cfd043268f7
Leon Malisov <leonmalisov@gmail.com>
2019-11-30 02:44:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -48,3 +48,8 @@ def over?(board)
     true
   end
 end
+
+def winner(board)
+  if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+    "X"
+     || board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
